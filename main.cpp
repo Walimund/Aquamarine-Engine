ffe1751cf7dae15fc783489f25c8876f29955d48
Walimund <wyatttmonroe@icloud.com>
2026-01-30 18:57:21 -0500

    Added first function

--- main.cpp
+++ main.cpp
@@ -2,17 +2,22 @@
 #include <string>
 
 // Displays the Engine information
+void systemInfo()
+{
+	std::string EngineName{ "Aquamarine Engine" };
+
+	std::string Version{ "1.0" };
+
+	bool Status{ true };
+	std::cout << std::boolalpha;
+
+	std::cout << EngineName << " is starting." << '\n' << "Version: " << Version
+		<< '\n' << "Engine Status: " << Status << '\n';
+}
+
 int main()
 {
-    std::string EngineName{ "Aquamarine Engine" };
+	systemInfo();
 
-    std::string Version{"1.0"};
-
-    bool Status{true};
-    std::cout << std::boolalpha;
-
-    std::cout << EngineName << " is starting." << '\n' << "Version: " << Version
-              << '\n' << "Engine Status: " << Status << '\n';
-
-    return 0;
-}                                                                                
+	return 0;
+}
