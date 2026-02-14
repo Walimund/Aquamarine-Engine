c2dc00e20d1e4bba236136c2bd72129c2e717a67
Walimund <wyatttmonroe@icloud.com>
2026-02-13 22:57:26 -0500

    Refactor: Implement namespace for game constants

---
+++ constants.h
@@ -0,0 +1,9 @@
+#pragma once
+
+namespace constants {
+
+	inline constexpr int baseCharacterHealth{ 100 };
+	inline constexpr int baseGoblinHealth{ 50 };
+	inline constexpr int baseGoblinDamage{ 10 };
+
+}
